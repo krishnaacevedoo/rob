083e944ccef028e9d58da964931dcdb5285fa67e
krishnaacevedoo <krishna.acevedo@estudiantes.colegiotgs.cl>
2025-10-06 15:14:39 -0300

    principal.py

--- principal.py
+++ principal.py
@@ -9,10 +9,11 @@ st.set_page_config(
 )
 
 # Configuración de Logo
-st.logo(
+st.image(
     "rob logo.JPG",
 )
 
 pg = st.navigation(["rob.py", "historia.py", "carrera.py", "música.py", "fandom.py", "quienes somos.py"])
 
 pg.run()
+
