57c765e23041719f84bb3f43112c9440cc74f067
krishnaacevedoo <krishna.acevedo@estudiantes.colegiotgs.cl>
2025-10-06 15:33:09 -0300

    Update carrera.py

--- carrera.py
+++ carrera.py
@@ -11,9 +11,10 @@ st.image("minecraftgif.gif", caption="Minecraft")
 
 # Más información y otras imágenes
 st.write("Actualmente cuenta con millones de suscriptores y transmite en diversas plataformas.")
-st.image("placa.WEBP", caption="Placas")  # Imagen de sus placas de YouTube
-st.image("kickk.WEBP", caption="Plataforma de stream")  # Imagen de Kick u otra plataforma
+st.image("placa.webp", caption="Placas")  # Imagen de sus placas de YouTube
+st.image("kickk.webp", caption="Plataforma de stream")  # Imagen de Kick u otra plataforma
 
 # Audio de fondo o efecto
 st.audio("sonidomine.mp3")  
+
 
